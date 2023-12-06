e769b5cc39fa2bb1393774ba0267a17540cf5edd
Tewan Ejoboja <ejobojat@gmail.com>
2023-12-06 15:12:58 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-# Function square_matrix_simple takes a matrix as input and computes a new matrix
-# by squaring each element of the input matrix.
+# Writes a function that computes the square value of all integers of a matrix.
 def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
     for idx, row in enumerate(new_matrix):
         for idx2, col in enumerate(new_matrix):
             new_matrix[idx][idx2] = row[idx2] ** 2
-    return new_matrix
+    return new_matrix 
